af908afc2a847b9be5736ef8d045c84afd714ba7
Malte Legenhausen <mlegenhausen@mailbox.org>
2026-08-17 08:54:32 +0200

    Change direnv cache location

--- users/mlegenhausen/programs/direnv.nix
+++ users/mlegenhausen/programs/direnv.nix
@@ -4,4 +4,18 @@
   enable = true;
   enableZshIntegration = true;
   nix-direnv.enable = true;
+
+  # Avoid cluttering project directories which often conflicts with tooling,
+  # as per:
+  #   https://github.com/direnv/direnv/wiki/Customizing-cache-location
+  stdlib = ''
+    : ''${XDG_CACHE_HOME:=$HOME/.cache}
+    declare -A direnv_layout_dirs
+    direnv_layout_dir() {
+      echo "''${direnv_layout_dirs[$PWD]:=$(
+        echo -n "$XDG_CACHE_HOME"/direnv/layouts/
+        echo -n "$PWD" | ${pkgs.coreutils}/bin/sha1sum | cut -d ' ' -f 1
+      )}"
+    }
+  '';
 }
